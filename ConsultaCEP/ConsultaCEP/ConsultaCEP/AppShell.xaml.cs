ef9124b52b603aafe561164b0cf7a37b1692d4f5
Luiz Fonsêca <antoniofonseca1991@gmail.com>
2021-04-17 19:19:52 -0300

    Criação inicial do projeto

---
+++ ConsultaCEP/ConsultaCEP/ConsultaCEP/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ConsultaCEP.ViewModels;
+using ConsultaCEP.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ConsultaCEP
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
